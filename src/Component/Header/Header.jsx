96b7b25770dff0b18a599a98c03c134758fd8449
Nour Amged <nouramgged2005@gmail.com>
2025-09-08 21:23:14 +0300

    finished header style

--- src/Component/Header/Header.jsx
+++ src/Component/Header/Header.jsx
@@ -1,17 +1,19 @@
 import styles from "./Header.module.css";
+import { IoCartOutline } from "react-icons/io5";
+import { CiShop } from "react-icons/ci";
 
-function Header(){
+function Header() {
     return (
         <header>
-            <div>
-                Shop Name
+            <div className={styles["shop-logo"]}>
+                <CiShop size={"22px"} /> What a Shop
             </div>
             <div className={styles["header-content"]}>
                 <p>Home</p>
                 <p>Shop</p>
                 <p>About</p>
-                <input type="text" />
-                <img src="" alt="logo" />
+                <input type="text" placeholder="Search products..." />
+                <IoCartOutline size={'22px'} />
             </div>
         </header>
     );
