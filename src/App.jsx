afc085e288ba3e3ef5be87fba64f0dd11ab697b7
Nour Amged <nouramgged2005@gmail.com>
2025-09-16 22:13:11 +0300

    started working on footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import Header from "./Component/Header/Header"
 import Main from "./Component/Main/Main";
+import Footer from "./Component/Footer/Footer";
 
 function App() {
     return (
         <>
             <Header />
             <Main />
+            <Footer />
         </>
     );
 }
---
+++ src/Component/Footer/Footer.jsx
@@ -0,0 +1,14 @@
+
+import styles from './Footer.module.css';
+
+function FooterSection() {
+    return (
+        <footer>
+            <div>
+             <p>Join our newsletter & get 20% off</p>   
+            </div>
+        </footer>
+    );
+}
+
+export default FooterSection;
