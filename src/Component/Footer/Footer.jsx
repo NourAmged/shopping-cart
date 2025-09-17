eca136fe438837bae45f3710a6380c9eb4424224
Nour Amged <nouramgged2005@gmail.com>
2025-09-17 20:37:40 +0300

    improved styles in footer

--- src/Component/Footer/Footer.jsx
+++ src/Component/Footer/Footer.jsx
@@ -4,8 +4,20 @@ import styles from './Footer.module.css';
 function FooterSection() {
     return (
         <footer>
-            <div>
-             <p>Join our newsletter & get 20% off</p>   
+            <div className={styles["subscribe"]}>
+                <h3>Join our newsletter & get 20% off</h3>
+                <p>Be the first to know about new arrivals,<br /> sales & exclusive offers.</p>
+                <form>
+                    <input
+                        type="email"
+                        id="email"
+                        name="email"
+                        placeholder="Enter your email"
+                        className={styles["email"]}
+                        required
+                    />
+                    <button type="submit" className={styles["subscribe-btn"]}>Subscribe</button>
+                </form>
             </div>
         </footer>
     );
