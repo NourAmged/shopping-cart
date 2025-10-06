d09eec4f22e0c5de9fc0757712713ce56533d068
Nour Amged <nouramgged2005@gmail.com>
2025-10-06 13:05:32 +0300

    added checkout summary and price calculation

--- src/Component/Main/Cart/Cart.jsx
+++ src/Component/Main/Cart/Cart.jsx
@@ -1,6 +1,17 @@
 import styles from './Cart.module.css';
 
 function Cart({ purchasedItem }) {
+
+    let subtotal = purchasedItem.reduce(
+        (price, item) => price + (item["price"] * item["amount"]),
+        0);
+
+    subtotal = Math.round(subtotal * 100) / 100;
+
+    let shipping = 0;
+
+    const totalCost = Math.round((subtotal + shipping) * 100) / 100;
+
     return (
         <div className={styles["cart"]}>
             <nav>
@@ -9,23 +20,48 @@ function Cart({ purchasedItem }) {
             <p style={{ fontSize: "1.5em", fontWeight: "550" }}>Your Shoping Cart</p>
             {
                 purchasedItem.length ?
-                    <div className={styles["items"]}>
-                        {purchasedItem.map((item, index) => {
-                            return (
-                                <div key={index}>
-                                    <div className={styles["item"]}>
-                                        <div className={styles["product-image-container"]}>
-                                            <img className={styles["product-image"]} src={item.image} alt="product item" />
+                    <div className={styles["product-container"]}>
+                        <div className={styles["items"]}>
+                            {purchasedItem.map((item, index) => {
+                                return (
+                                    <div key={index}>
+                                        <div className={styles["item"]}>
+                                            <div className={styles["product-image-container"]}>
+                                                <img className={styles["product-image"]} src={item.image} alt="product item" />
+                                            </div>
+                                            <div>
+                                                <p>{item.title}</p>
+                                                <p>${item.price}</p>
+                                                <p>quantity: {item.amount}</p>
+                                            </div>
                                         </div>
-                                        <div>
-                                            <p>{item.title}</p>
-                                            <p>${item.price}</p>
-                                        </div>
-                                    </div>
 
+                                    </div>
+                                );
+                            })}
+                        </div>
+                        <div className={styles["checkout-summary"]}>
+                            <div className={styles["summary"]}>
+                                <p className={styles["summary-header"]}>Order Summary</p>
+                                <div className={styles["subtotal"]}>
+                                    <p>Subtotal</p>
+                                    <p>${subtotal}</p>
                                 </div>
-                            );
-                        })}
+                                <div className={styles["shipping"]}>
+                                    <p>Shipping</p>
+                                    <p>Free</p>
+                                </div>
+                            </div>
+                            <div>
+                                <div className={styles["total"]}>
+                                    <p>Total</p>
+                                    <p>${totalCost}</p>
+                                </div>
+                                <div className={styles["checkout-btn-container"]}>
+                                    <button className={styles["checkout-btn"]}>Proceed to Checkout</button>
+                                </div>
+                            </div>
+                        </div>
                     </div>
                     :
                     <div className={styles["no-items"]}>
