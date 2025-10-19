0fd3963213360ee5b1c6a50267e466d0a8798eff
Nour Amged <nouramgged2005@gmail.com>
2025-10-19 16:50:00 +0300

    starting to make the layout responsive

--- src/Component/Header/Header.jsx
+++ src/Component/Header/Header.jsx
@@ -1,6 +1,7 @@
 import styles from "./Header.module.css";
 import { IoCartOutline } from "react-icons/io5";
 import { CiShop } from "react-icons/ci";
+import { IoMenuSharp } from "react-icons/io5";
 import { useNavigate } from "react-router";
 
 function Header({ purchasedItem }) {
@@ -15,17 +16,20 @@ function Header({ purchasedItem }) {
             <button className={styles["shop-logo"]} onClick={() => navigate("/")}>
                 <CiShop size={'32px'} /> What a Shop
             </button>
-            <div className={styles["header-content"]}>
-                <button className={styles["home-btn"]} onClick={() => navigate("/")}>Home</button>
-                <button className={styles["shop-btn"]}>Shop</button>
-                <button className={styles["about-btn"]}>About</button>
-                <input type="text" placeholder="Search products..." />
-                <div>
+            <div style={{ display: "flex", gap: "12px" }}>
+                <div className={styles["header-content"]}>
+                    <button className={styles["home-btn"]} onClick={() => navigate("/")}>Home</button>
+                    <button className={styles["shop-btn"]}>Shop</button>
+                    <button className={styles["about-btn"]}>About</button>
+                    <input type="text" placeholder="Search products..." />
+                </div>
+                <span>
                     <IoCartOutline style={{ cursor: "pointer" }} onClick={() => navigate("/cart")} size={'24px'} />
                     {
                         totalAmount === 0 ? "" : <span className={styles["total-amount"]}>{totalAmount}</span>
                     }
-                </div>
+                    <IoMenuSharp size={26} className={styles["drawer"]}/>
+                </span>
             </div>
         </header>
     );
